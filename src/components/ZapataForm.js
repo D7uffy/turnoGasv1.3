4450ed13d50ed8d9c4f97ecb8f05c36d0a66302e
peter <pprancel@gmail.com>
2023-05-27 15:18:55 -0400

    act zapata 4215

--- src/components/ZapataForm.js
+++ src/components/ZapataForm.js
@@ -3,7 +3,7 @@ import swal from "sweetalert";
 
 
 const ZapataForm = () => {
-  const turnoPista = 4054;
+  const turnoPista = 4215;
   const promediodeturnosPorDia = 233;
 
   const calcularTurnosFaltantes = (turnoPersona, turnoPista) => {
