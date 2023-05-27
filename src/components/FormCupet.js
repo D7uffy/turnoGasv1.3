4b2388620613bac2e4a7faaef5a63fd543ba017c
peter <pprancel@gmail.com>
2023-05-27 15:50:30 -0400

    tratando de añadir img al fomr

--- src/components/FormCupet.js
+++ src/components/FormCupet.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import TanganaForm from "./TanganaForm";
 import ZapataForm from "./ZapataForm";
 import "./Style.css";
-import logo from "../images/cupetTurnosLogo.png";
+//import logo from 'process.env.PUBLIC_URL';
 
 const FormCupet = () => {
   const [visible, setvisible] = useState(false);
@@ -21,7 +21,8 @@ const FormCupet = () => {
       <div>
         {" "}
         <h1 id="texto">Elija un </h1>
-        <img src={logo}  alt="TurnoCupet" width={60} height={60} />
+        <img src={process.env.PUBLIC_URL + "/img/cupetTurnosLogo.png "}  alt="TurnoCupet" width={60} height={60} />
+        <hr/>
         <cite>Act. 27-05-2023 15:00</cite>
       </div>
 
